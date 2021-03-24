106b15d1a6fb3c512c5a1320ac342c3616b3511c
Ender Ruiz <eruiz@MacBook-Pro-de-Ender.local>
2021-03-24 00:00:15 -0300

    Adding html with photos, Nasa

--- prueba_final.rb
+++ prueba_final.rb
@@ -14,7 +14,28 @@ response = https.request(request)
 
 documents = JSON.parse (response.read_body)
 
+nasa_img = []
 
+documents["photos"].count.times {|x| nasa_img.push documents["photos"][x]["img_src"]}
+
+list = ""
+nasa_img.count.times {|x| list += "<li><img src=#{nasa_img[x]}></li>" } 
+
+last_html = '<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>' +
+"</head>
+<body>
+<ul>
+    #{list}
+</ul>
+</body>
+</html>"
+File.write('index.html', last_html)
 
 
 
